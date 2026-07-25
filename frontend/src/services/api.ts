66f2fe89cfc6cb4851e0ce2b7ad5c41b0632b728
Awais Majeed <awais@gmail.com>
2026-07-26 01:59:14 +0500

    update pharmacy system

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -1,33 +1,4 @@
-// import axios from "axios";
-
-// const api = axios.create({
-//   baseURL: "https://pharmacysystem-production-a423.up.railway.app/api/v1",
-// });
-
-// api.interceptors.request.use((config) => {
-//   const token = localStorage.getItem("token");
-//   if (token) {
-//     config.headers.Authorization = `Bearer ${token}`;
-//   }
-//   return config;
-// });
-
-// api.interceptors.response.use(
-//   (response) => response,
-//   (error) => {
-//     if (error.response?.status === 401) {
-//       localStorage.removeItem("token");
-//       localStorage.removeItem("user");
-//       window.location.href = "/login";
-//     }
-//     return Promise.reject(error);
-//   }
-// );
-
-// export default api;
 import axios from "axios";
-
-alert("THIS IS THE NEW API.TS");
 
 const api = axios.create({
   baseURL: "https://pharmacysystem-production-a423.up.railway.app/api/v1",
@@ -40,5 +11,17 @@ api.interceptors.request.use((config) => {
   }
   return config;
 });
+
+api.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    if (error.response?.status === 401) {
+      localStorage.removeItem("token");
+      localStorage.removeItem("user");
+      window.location.href = "/login";
+    }
+    return Promise.reject(error);
+  }
+);
 
 export default api;
